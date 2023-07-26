0a3aac538e79dca47bc04cd6d7b9712be2174fc6
RashanWeerasinghe <malithrashan03@gmail.com>
2023-07-26 23:01:42 +0530

    Register Component

--- Frontend/src/Components/SignUp/SignUp.js
+++ Frontend/src/Components/SignUp/SignUp.js
@@ -1,9 +1,65 @@
-import React from "react";
+import { React, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import FormInput from "../common/FormInput";
+import SubmitButton from "../common/SubmitButton";
+import { api } from "../../config";
+import "../SignUp/SignUp.css";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      if (confirmPassword !== password) {
+        toast.error("Passwords don't match");
+        return;
+      }
+
+      await axios.post(`${api}/auth/register`, {
+        email,
+        password,
+      });
+
+      toast.success("Registration Successful");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="center-div">
+      <form className="register-container">
+        <h2>Register</h2>
+        <FormInput
+          label="Email Address"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <SubmitButton text="Submit" onClick={handleSubmit} />
+      </form>
     </div>
   );
 };
