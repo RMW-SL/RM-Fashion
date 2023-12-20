02cec0840c4029dcb07cef83da709cdfb3237381
RashanWeerasinghe <malithrashan03@gmail.com>
2023-12-21 00:26:57 +0530

    remove folder

--- RM-Fashion/src/main/java/com/rmfashion/RM/Fashion/Dto/ResponseDTO/ResponseUserDTO.java
+++
@@ -1,4 +0,0 @@
-package com.rmfashion.RM.Fashion.Dto.ResponseDTO;
-
-public class ResponseUserDTO {
-}
--- RM-Fashion/src/main/java/com/rmfashion/RM/Fashion/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.rmfashion.RM.Fashion.Service;
-
-import com.rmfashion.RM.Fashion.Dto.RequestDTO.RequestUserDTO;
-import org.springframework.stereotype.Service;
-
-
-public interface UserService {
-    String save(RequestUserDTO requestUserDTO);
-}
